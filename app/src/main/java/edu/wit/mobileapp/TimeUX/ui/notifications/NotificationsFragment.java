6edea683da9ff56a7cb63acfd7d953d73e817116
neupanep <neupanep@wit.edu>
2020-04-02 09:00:30 -0400

    settings error

---
+++ app/src/main/java/edu/wit/mobileapp/TimeUX/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,26 @@
+package edu.wit.mobileapp.TimeUX.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import edu.wit.mobileapp.TimeUX.R;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        return root;
+    }
+}
