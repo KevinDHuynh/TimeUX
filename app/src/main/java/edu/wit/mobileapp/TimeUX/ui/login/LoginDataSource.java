036733fc75f356757f3978a8420023898097aca7
Kevin Huynh <huynhk1@wit.edu>
2020-04-02 10:51:49 -0400

    initial http implementaiton for login

--- app/src/main/java/edu/wit/mobileapp/TimeUX/ui/login/LoginDataSource.java
+++ app/src/main/java/edu/wit/mobileapp/TimeUX/ui/login/LoginDataSource.java
@@ -1,9 +1,12 @@
 package edu.wit.mobileapp.TimeUX.ui.login;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
 import java.io.IOException;
+import java.util.UUID;
 
 import edu.wit.mobileapp.TimeUX.model.LoggedInUser;
-import kong.unirest.Unirest;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -14,14 +17,17 @@ public class LoginDataSource {
 
         try {
             // TODO: handle loggedInUser authentication
-            Unirest.get("137.135.120.16:8080")
-                    .basicAuth(username,password)
-                    .asString();
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
+            HttpResponse<String> response =
+                    Unirest.get("137.135.120.16:8080/users")
+                            .basicAuth(username, password)
+                            .asString();
+            if (response.getCode() == 200) {
+                LoggedInUser fakeUser =
+                        new LoggedInUser(
+                                UUID.randomUUID().toString(),
+                                "Jane Doe");
+                return new Result.Success<>(fakeUser);
+            } else throw new IOException();
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
